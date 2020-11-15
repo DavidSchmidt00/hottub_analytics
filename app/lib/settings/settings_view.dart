55a5c7dd0e5fabc82411f8d625de7d335a7ae553
DavidSchmidt00 <davod2211@gmail.com>
2020-11-15 18:44:56 +0100

    added build statement to docker-compose

--- app/lib/settings/settings_view.dart
+++ app/lib/settings/settings_view.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../settings/settings.dart' as settings;
+
 class SettingsView extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Einstellungen"),
         centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          Card(
+            child: ListTile(
+              title: Text(settings.BACKEND_URL),
+              trailing: IconButton(icon: Icon(Icons.edit), onPressed: () {},),
+            ),
+
+          ),
+        ],
       ),
     );
   }
